561d8c7eeae6059ad85bbdc3c94d82fbe78639c9
AndreiMuresan246 <92537159+AndreiMuresan246@users.noreply.github.com>
2021-10-14 19:07:02 +0300

    Add files via upload

---
+++ L3_P1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+int functie(double i)
+{
+    double r;
+    r = (pow(i,4)) - (5*pow(i,2)) + (3*i) + 20;
+    return r;
+}
+int main()
+{
+    double n, m;
+    double r, eps = 0.001, minim = INT_MAX, val_minim, i;
+    printf("Introduce-ti valorile intervalului in care f(x) exista ");
+    scanf("%lf", &n);
+    scanf("%lf", &m);
+    for(i = n; i <= m; i = i + eps)
+    {
+        r = functie(i);
+        if (minim > r)
+        {
+            minim = r;
+            val_minim = i;
+        }
+    }
+    printf("Valoarea pentru care functia f(x) este minima este x = %lf, iar valoarea functiei f(%lf) este %lf", val_minim, val_minim, minim);
+    return 0;
+}
